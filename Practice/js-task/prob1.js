100ca873773a59f9b631df9bf880716bc8add495
rupaliwakhare <rupaliwakhare184@gmail.com>
2026-04-09 11:14:49 +0530

    prob1

---
+++ Practice/js-task/prob1.js
@@ -0,0 +1,19 @@
+// let a = 10;
+// console.log(a);
+
+// let a = 30;
+// let b = 50;
+// console.log(b-a);
+
+// let a = 3;
+// let b = 4;
+// console.log(Math.floor(b/3));
+
+// var a = 19;
+// var b = 40;
+// console.log(b-a);
+
+//  const a = 30;
+//  const b = 40;
+//  console.log(a+b);
+ 
