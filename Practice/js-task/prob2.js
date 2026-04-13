a2442a8269018ad25a0c74e780a18375a6cfa12a
rupaliwakhare <rupaliwakhare184@gmail.com>
2026-04-13 20:09:17 +0530

    prob2

--- Practice/js-task/prob2.js
+++ Practice/js-task/prob2.js
@@ -6,6 +6,15 @@
 
 const c = 30;
 const d = 20;
-console.log(typeof(c<=d));
+console.log(typeof (c <= d));
 
+let b = "number";
+console.log(typeof b);
+
+value();
+//  let a = 30;
+function value() {
+  let a = 30;
+  console.log(a);
+}
 
