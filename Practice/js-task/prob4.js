130a73db1a5e00c59db799d8fc53f2d468a64bdd
rupaliwakhare <rupaliwakhare184@gmail.com>
2026-04-13 21:04:22 +0530

    prob4

---
+++ Practice/js-task/prob4.js
@@ -0,0 +1,14 @@
+let age = 60;
+
+if (age >= 60) {
+  console.log("Senior");
+} else if (age >= 18) {
+  console.log("Adult");
+} else if (age < 18) {
+  console.log("Minor");
+} else {
+  console.log("Not found");
+}
+
+
+//Senior
