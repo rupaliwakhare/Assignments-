041609011bbcf5eff665266f544910371b030cfc
rupaliwakhare <rupaliwakhare184@gmail.com>
2026-04-16 15:02:38 +0530

    prob

--- Practice/js-task/Array/prob1.js
+++ Practice/js-task/Array/prob1.js
@@ -1,14 +1,29 @@
+// pop  remove element last
+
+
+
 // let arr = [1,2,3,4,5]
 // arr.pop()
-// // console.log(arr);
+// console.log(arr); //[1,2,3,4]
+
+ 
+ 
+// push  Add elemeng last
+
 
 // let arr = [11, 22, 33, 44, 55, 66];
 // arr.push(67);
-// console.log(arr);
+// console.log(arr); // [11,22,33,44,55,67]
 
+
+// shift  remove element starts
 // let arr = [11, 22, 33, 44, 55, 66];
 // arr.shift();
-// console.log(arr);
-let arr = [11, 22, 33, 44, 55, 66];
-arr.unshift(67);
-console.log(arr);
+// console.log(arr); // [22,33,44,55,66]
+
+
+
+// unshift Add element start
+// let arr = [11, 22, 33, 44, 55, 66];
+// arr.unshift(67);
+// console.log(arr); //[Add element starts]
