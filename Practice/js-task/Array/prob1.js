ded72fbe8fa7bbfc9757d11a6067cb16d8e6d480
rupaliwakhare <rupaliwakhare184@gmail.com>
2026-04-16 14:56:08 +0530

    prob1

---
+++ Practice/js-task/Array/prob1.js
@@ -0,0 +1,14 @@
+// let arr = [1,2,3,4,5]
+// arr.pop()
+// // console.log(arr);
+
+// let arr = [11, 22, 33, 44, 55, 66];
+// arr.push(67);
+// console.log(arr);
+
+// let arr = [11, 22, 33, 44, 55, 66];
+// arr.shift();
+// console.log(arr);
+let arr = [11, 22, 33, 44, 55, 66];
+arr.unshift(67);
+console.log(arr);
