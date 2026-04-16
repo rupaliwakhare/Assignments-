a5a9a42844485ad29480acfd43a4f49393be22fc
rupaliwakhare <rupaliwakhare184@gmail.com>
2026-04-16 13:03:48 +0530

    prob1.js

---
+++ Practice/practice/prob1.js
@@ -0,0 +1,24 @@
+// let obj={
+//     math:"96",
+//     bio:"89",
+//     phy:"67",
+//     en:"56"
+// }
+
+// let sum = 0
+
+// for(let key in obj){
+//     sum +=Number(obj[key])
+// }
+// console.log(sum);
+
+let marks={
+    marathi:"23",
+    eng:"12"
+
+}
+let sum = 0
+for(let key in marks){
+sum +=Number(marks[key])
+}
+console.log(sum);
