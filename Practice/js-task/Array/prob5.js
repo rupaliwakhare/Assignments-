ac3f948a56e3c6d36ca8a0b019fc50e76c4e2f58
rupaliwakhare <rupaliwakhare184@gmail.com>
2026-04-17 12:04:32 +0530

    prob-obj

--- Practice/js-task/Array/prob5.js
+++ Practice/js-task/Array/prob5.js
@@ -1,14 +1,16 @@
 const student = {
   name: "Ravi",
   age: 20,
-  grade: "A"
+  grade: "A",
 };
 
-
 console.log(student["name"]);
 
-student.age=21;
+student.age = 21;
 console.log(student.age);
-student.name="Ruhani"
+student.name = "Ruhani";
 console.log(student["name"]);
 
+student.subject = "Maths";
+student.city = "Pune";
+console.log(student);
