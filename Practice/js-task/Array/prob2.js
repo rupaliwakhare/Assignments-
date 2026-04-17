3c2e37f4e45f914c3a2d4dc639fe51cb1e399583
rupaliwakhare <rupaliwakhare184@gmail.com>
2026-04-17 11:21:05 +0530

    array-double

---
+++ Practice/js-task/Array/prob2.js
@@ -0,0 +1,10 @@
+// let number = [2,3,4,5,6]
+
+
+// const double = number.map(num=>num*2)
+// console.log("originam array",number);
+// console.log("double Array",double);
+
+// let array = [34,56,78,46]
+// const double = array.map(num=>num*2)
+// console.log(double);
