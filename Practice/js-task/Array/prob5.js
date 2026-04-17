d0a25d019819b9caee881922393083727c5367e0
rupaliwakhare <rupaliwakhare184@gmail.com>
2026-04-17 12:01:27 +0530

    prob5

---
+++ Practice/js-task/Array/prob5.js
@@ -0,0 +1,14 @@
+const student = {
+  name: "Ravi",
+  age: 20,
+  grade: "A"
+};
+
+
+console.log(student["name"]);
+
+student.age=21;
+console.log(student.age);
+student.name="Ruhani"
+console.log(student["name"]);
+
