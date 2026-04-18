fddeca6f76adbefbdc01e43b08444292e1a70d45
rupaliwakhare <rupaliwakhare184@gmail.com>
2026-04-18 14:36:32 +0530

    prob3

---
+++ Practice/js-task/fun/prob3.js
@@ -0,0 +1,13 @@
+// function add(a, b) {
+//   return a + b;
+// }
+
+
+// const add = (a, b) => {
+//   return a + b;
+// };
+
+const add = (a, b) => a + b;
+
+console.log(add(4, 6));
+// console.log(add(3,5));
