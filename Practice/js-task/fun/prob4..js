3a979902137ee6516797beddceb3c536db9b21e9
rupaliwakhare <rupaliwakhare184@gmail.com>
2026-04-20 16:03:32 +0530

    prob4

---
+++ Practice/js-task/fun/prob4..js
@@ -0,0 +1,15 @@
+// What is Hoisting?
+
+// Hoisting means JavaScript moves declarations to the top of their scope before execution.
+
+// But the behavior is different for var, let, and const.
+
+// console.log(a);
+// var a = 10;   // undefined 
+
+let text = "javascript";
+
+console.log(text.slice(0,4));
+console.log(text.slice(4)); 
+
+
