e1a61c590964e4e9dc9e4d183afb32caa85b7427
rupaliwakhare <rupaliwakhare184@gmail.com>
2026-05-11 12:34:05 +0530

    prob1

---
+++ Practice/trikey/prob1.js
@@ -0,0 +1,4 @@
+// console.log([]===[]); // false beacouse 2 different array dont share same reference.
+// console.log([]==={});   // false beacouse ckeck reference equality do not check value equality.
+
+ console.log([1,2] + [3,4]); //  array are converted into string and concaninate
